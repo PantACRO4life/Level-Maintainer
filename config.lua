a7edda82a83be1264adac8404e12bc907c19f0b4
Armagedon13 <uly.kapo@gmail.com>
2025-11-08 08:07:00 -0300

    fix config.lua example

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ local cfg = {}
 
 -- ["item_name"] = {{item_id, item_meta},threshold, batchsize} -- keep in mind that no threshold has a better performance!
 -- ["fluid_name"] = {{fluid_tag item},threshold, batchsize}}  -- keep in mind that no threshold has a better performance!
--- ["Osmium Dust"] = {{ item_id = "gregtech:gt.metaitem.01", item_meta = "2083"}, 1000, 64}, -- without threshold, batch_size=64
+-- ["Osmium Dust"] = {{ item_id = "gregtech:gt.metaitem.01", item_meta = "2083"}, nil, 64}, -- without threshold, batch_size=64
 -- ["drop of Molten SpaceTime"] = {{ fluid_tag = "molten.spacetime"}, 1000, 64} -- with threshold, batch_size=64
 
 cfg["items"] = {
